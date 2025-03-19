6a3e36a0b3648cade0ea59ab6a6f0b6b3a5f0973
Camilo Game <tenorio2003@gmail.com>
2025-03-19 13:01:05 -0500

    add_shortDesc

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react"
+
 const CardImage = ({image,desc}) => {
+
+    const [shortDesc, setShortDesc] = useState();
+
+    const descShortener = (desc) => {
+        return desc.split(" ").slice(0, 4).join(" ");
+    }
+
+    useEffect(()=>{
+        setShortDesc(descShortener(desc))
+    }, [image])
 
     return(
         <div className="card-image-container">
             <img src={image}></img>
-            <p>{desc}</p>
+            <p>{shortDesc}</p>
         </div>
     )
 }
